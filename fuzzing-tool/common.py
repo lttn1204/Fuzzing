660fef4336be2e0ff2a40901eb0bc4e8c244bf6d
lttn1204 <19520799@gm.uit.edu.vn>
2022-11-23 14:39:35 +0700

    up

--- fuzzing-tool/common.py
+++ fuzzing-tool/common.py
@@ -4,14 +4,13 @@ import datetime
 import json
 import re
 
-
 class ExecStatus(Enum):
     NORMAL = 0xd0
     CRASH = NORMAL + 1
     ABORT = NORMAL + 2
     DOS = NORMAL + 3
 
-
+    
 class TraceInfo:
     def __init__(self, module_name, bb_list):
         self.module_name = module_name
@@ -63,119 +62,6 @@ class ExecResult:
     def __str__(self):
         return "status: {}, crash_info: {}".format(self.status, self.crash_info)
 
-'''
-class Testcase:
-    def __init__(self, idx, bb_executed):
-        self.idx = idx
-        self.trace = bb_executed
-        self.base_exec_count = 50
-        self.exec_count = self.base_exec_count
-        self.path_found = 0  # new path found by this case
-        self.dos_count = 0
-
-        self.crash_count = 0
-
-        self.seed_id = 0
-        self.seed_hash = ""
-
-        self.inc_ratio = 0.1
-        self.dec_ratio = 0.1
-
-    def get_trace(self):
-        return self.trace
-
-    def get_trace_count(self):
-        count = 0
-        for ti in self.trace:
-            count += len(ti.bb_list)
-        return count
-
-    def is_contain_by(self, seed):
-        for i in range(self.trace):
-            a = set(self.trace[i].bb_list)
-            b = set(seed.trace[i].bb_list)
-            if not b.issuperset(a):
-                return False
-        return True
-
-    def found_crash(self):
-        self.crash_count += 1
-
-    def found_dos(self):
-        self.exec_count = int(self.exec_count * self.dec_ratio)
-        if self.exec_count == 0:
-            self.exec_count = 1
-        self.dos_count += 1
-
-    def found_path(self):
-        self.path_found += 1
-        self.exec_count = int(self.base_exec_count + self.base_exec_count * self.path_found * self.inc_ratio)
-
-    def __str__(self):
-        data = "idx: {}, crash found: {}, dos found: {}, path found: {}, bb count: {}, exec count: {}".format(
-            self.idx, self.crash_count, self.dos_count, self.path_found, self.get_trace_count(), self.exec_count)
-        return data
-'''
-
-class Crash:
-    def __init__(self, idx, trace, crash_info=""):
-        self.idx = idx
-        self.trace = trace
-        self.crash_info = crash_info
-
-        data = ""
-        for ti in trace:
-            data += "{}\n".format(ti.module_name)
-            data += ','.join(['{:X}'.format(x) for x in ti.bb_list])
-            data += '\n'
-
-        crash_hash = self.parse_crash_hash(crash_info)
-
-        if crash_hash:
-            self.trace_hash = crash_hash[:18]  # max depth is 6
-        else:
-            self.trace_hash = hashlib.md5(data + crash_info).hexdigest()
-
-    def parse_crash_hash(self, crash_info):
-        pc = None
-        try:
-            d = re.findall("crash-hash: (.*?) ", crash_info)
-            if len(d) > 0:
-                return d[0]
-
-            d = re.findall("eip=(.*?) ", crash_info)
-            if len(d) > 0:
-                return d[0][-3:]
-
-            d = re.findall("rip\s+(.*?) ", crash_info)
-            if len(d) > 0:
-                return d[0][-3:]
-
-            d = re.findall("eip\s+(.*?) ", crash_info)
-            if len(d) > 0:
-                return d[0][-3:]
-        except Exception as e:
-            print ("get_crash_hash_from_crash_info failed")
-            print (e)
-            print (crash_info)
-        return pc
-
-
-class Dos:
-    def __init__(self, idx, trace, exec_time=-1):
-        self.idx = idx
-        self.trace = trace
-
-        data = ""
-
-        for ti in trace:
-            data += "{}\n".format(ti.module_name)
-            data += ','.join(['{:X}'.format(x) for x in ti.bb_list])
-            data += '\n'
-
-        self.trace_hash = hashlib.md5(data).hexdigest()
-
-
 class CustomLogger:
     def __init__(self):
         pass
@@ -195,6 +81,7 @@ class EdgeInfo:
         self.from_bb=from_bb
         self.to_bb=to_bb
         self.value=1
+
     def __gt__(self,other):
         if self.from_bb > other.from_bb:
             return True
@@ -202,6 +89,7 @@ class EdgeInfo:
             if self.to_bb>other.to_bb:
                 return True
         return False
+
     def __lt__(self,other):
         if self.from_bb < other.from_bb:
             return True
@@ -209,19 +97,16 @@ class EdgeInfo:
             if self.to_bb<other.to_bb:
                 return True
         return False
+
     def __eq__(self,other):
         if self.from_bb == other.from_bb and self.to_bb==other.to_bb:
             return True
         return False
+
     def __str__(self):
         print(f"{self.from_bb} - {self.to_bb}: {self.value}")
 
 
-def gen_original_seed(length):
-    arr=[]
-    for i in range(length):
-        arr.append(i%256)
-    return bytes(arr)
 
 def calculate_coverage_similaty_and_frequency_difference(edges1,edges2):
     numerator_cs=0
@@ -257,7 +142,7 @@ def check_same_field(begin_offset, end_offset, info_mutate_seed):
             return False
     return True
 
-def check_exit_value_1_and_other_lessthan_alpha(offset,info_mutate_seed,alpha):#for assertion_field_identification
+def check_exit_value_1_and_other_lessthan_alpha(offset,info_mutate_seed):#for assertion_field_identification
     count=0
     for i in range(256):
         if info_mutate_seed[offset]["coverage_similaty"][i]==1:
@@ -265,13 +150,13 @@ def check_exit_value_1_and_other_lessthan_alpha(offset,info_mutate_seed,alpha):#
             if count>1:
                 return False
         else:
-            if info_mutate_seed[offset]["coverage_similaty"][i]>alpha:
+            if info_mutate_seed[offset]["coverage_similaty"][i]>info_mutate_seed[offset]["alpha"]:
                 return False
     return True
  
-def assertion_field_identification(begin_offset, end_offset, info_mutate_seed,alpha):
+def assertion_field_identification(begin_offset, end_offset, info_mutate_seed):
     for x in range(begin_offset,end_offset):
-        if check_exit_value_1_and_other_lessthan_alpha(x,info_mutate_seed,alpha)==False:
+        if check_exit_value_1_and_other_lessthan_alpha(x,info_mutate_seed)==False:
             return False
     return True
 
@@ -282,32 +167,32 @@ def check_all_value_is_1(offset,info_mutate_seed): #for raw_data_field_identific
     return True
 
 
-def raw_data_field_identification(begin_offset, end_offset, info_mutate_seed,alpha):
+def raw_data_field_identification(begin_offset, end_offset, info_mutate_seed):
     for offset in range(begin_offset,end_offset):
         if check_all_value_is_1(offset,info_mutate_seed)==False:
             return False
     return True
 
-def check_for_identy_subpace_for_enumeration(offset,info_mutate_seed,i,j,alpha):#for identy_subpace_for_enumeration
+def check_for_identy_subpace_for_enumeration(offset,info_mutate_seed,i,j):#for identy_subpace_for_enumeration
     for k in range(i,256):
         if k<j:
-            if info_mutate_seed[offset]["coverage_similaty"][k]<alpha:
+            if info_mutate_seed[offset]["coverage_similaty"][k]<info_mutate_seed[offset]["alpha"]:
                 return False
         else:
-            if info_mutate_seed[offset]["coverage_similaty"][k]<alpha:
+            if info_mutate_seed[offset]["coverage_similaty"][k]>info_mutate_seed[offset]["alpha"]:
                 return False
     return True
 
-def identy_subpace_for_enumeration(offset,info_mutate_seed,alpha): #for enumeration_field_indentification
+def identy_subpace_for_enumeration(offset,info_mutate_seed): #for enumeration_field_indentification
     for i in range(1,256):
         for j in range(i+1,256):
-        if check_for_identy_subpace_for_enumeration:
-            return True
+            if check_for_identy_subpace_for_enumeration(offset,info_mutate_seed,i,j):
+                return True
     return False
 
-def enumeration_field_indentification(begin_offset, end_offset, info_mutate_seed,alpha):
+def enumeration_field_indentification(begin_offset, end_offset, info_mutate_seed):
     for x in range(begin_offset,end_offset):
-        if identy_subpace_for_enumeration(offset,info_mutate_seed,alpha):
+        if identy_subpace_for_enumeration(x,info_mutate_seed):
             return True
     return False
 
@@ -323,10 +208,10 @@ def variance_coverae_similaty(offset,info_mutate_seed):#for loop_count_field_ind
 def average_frequency_difference(offset,info_mutate_seed):#for loop_count_field_indentification
     return sum(info_mutate_seed[offset]["frequency_difference"])/len(info_mutate_seed[offset]["frequency_difference"])
 
-def loop_count_field_indentification(begin_offset, end_offset, info_mutate_seed,alpha):
+def loop_count_field_indentification(begin_offset, end_offset, info_mutate_seed):
     beta=7.355
     for x in range(begin_offset,end_offset):
-        if variance_coverae_similaty(x,info_mutate_seed)<beta and average_frequency_difference(offset,info_mutate_seed)>1:
+        if variance_coverae_similaty(x,info_mutate_seed)<beta and average_frequency_difference(x,info_mutate_seed)>1:
             return True
     return False
 
@@ -339,31 +224,31 @@ def check_u_v_difference(offset,info_mutate_seed,index):#for identy_subpace
                 return True
     return False
 
-def check_other_value_lt_alpha(offset,info_mutate_seed,index,alpha):#for identy_subpace
+def check_other_value_lt_alpha(offset,info_mutate_seed,index):#for identy_subpace
     for i in range(index):
-        if info_mutate_seed[offset]["coverage_similaty"][i]>alpha:
+        if info_mutate_seed[offset]["coverage_similaty"][i]>info_mutate_seed[offset]["alpha"]:
             return False
-    retunr True
+    return True
 
-def identy_subpace(offset,info_mutate_seed,alpha): #for offset_field_indentification and size_field_indentification
+def identy_subpace(offset,info_mutate_seed): #for offset_field_indentification and size_field_indentification
     for i in range(3,256):
-        if check_u_v_difference and check_other_value_lt_alpha(offset,info_mutate_seed,index,alpha):
+        if check_u_v_difference(offset,info_mutate_seed,i) and check_other_value_lt_alpha(offset,info_mutate_seed,i):
             return True
     return False
 
 
-def check_value_index0_lt_alpha(offset,info_mutate_seed,alpha):#for offset_field_indentification and size_field_indentification
-    return info_mutate_seed[offset]["coverage_similaty"][0]>alpha
+def check_value_index0_lt_alpha(offset,info_mutate_seed):#for offset_field_indentification and size_field_indentification
+    return info_mutate_seed[offset]["coverage_similaty"][0]>info_mutate_seed[offset]["alpha"]
 
-def offset_field_indentification(begin_offset, end_offset, info_mutate_seed,alpha):
+def offset_field_indentification(begin_offset, end_offset, info_mutate_seed):
     for x in range(begin_offset,end_offset):
-        if check_value_index0_lt_alpha(x,info_mutate_seed,alpha) and identy_subpace(x,info_mutate_seed,alpha):
+        if check_value_index0_lt_alpha(x,info_mutate_seed) and identy_subpace(x,info_mutate_seed):
             return True
     return False
 
-def size_field_indentification(begin_offset, end_offset, info_mutate_seed,alpha):
+def size_field_indentification(begin_offset, end_offset, info_mutate_seed):
     for x in range(begin_offset,end_offset):
-        if not check_value_index0_lt_alpha(x,info_mutate_seed,alpha) and identy_subpace(x,info_mutate_seed,alpha):
+        if not check_value_index0_lt_alpha(x,info_mutate_seed) and identy_subpace(x,info_mutate_seed):
             return True
     return False
 
--- fuzzing-tool/fuzzer.py
+++ fuzzing-tool/fuzzer.py
@@ -2,18 +2,18 @@ from get_all_basic_block import get_all_basic_block
 from patch_all_basic_block import patch_bb
 from mutator_for_get_info import info_mutator
 from tracer import PythonPtraceTracer
-from common import calculate_coverage_similaty_and_frequency_difference
+from common import *
 
 ida_path = "/home/lttn/Fuzzing/IDA7.7/IDA7.7/ida64.exe"
 file_path = "/home/lttn/Fuzzing/Target/imgread"
-input_length = 2
+input_length = 4
 #get_all_basic_block(ida_path,file_path)
 #patch_bb(file_path,file_path+"-bb.txt")
 
 
 info_mutate_seed={}
 
-length_seed=input_length
+length_seed=input_length+1
 original_seed=bytes([i%256 for i in range(length_seed)])
 w=open("tmp","wb")
 w.write(original_seed)
@@ -39,7 +39,7 @@ while True:
         info_mutate_seed[idx-1]={}
         info_mutate_seed[idx-1]["coverage_similaty"]=coverage_similaty
         info_mutate_seed[idx-1]["frequency_difference"]=frequency_difference
-        nfo_mutate_seed[idx-1]["alpha"]=alpha
+        info_mutate_seed[idx-1]["alpha"]=alpha
         generator_input.is_new_idx=False
 
     w=open("tmp","wb")
@@ -51,20 +51,73 @@ while True:
     info_per_bytes[generator_input.value]=edge_info
 
     if generator_input.is_done:
+        print("catch done")
         coverage_similaty=[]
         frequency_difference=[]
         for value in range(256):
             tmp1,tmp2=calculate_coverage_similaty_and_frequency_difference(original_info,info_per_bytes[value])
             coverage_similaty.append(tmp1)
+            alpha=(max(coverage_similaty)+min(coverage_similaty))/2
             frequency_difference.append(tmp2)
         info_per_bytes={}
         info_mutate_seed[idx-1]={}
         info_mutate_seed[idx-1]["coverage_similaty"]=coverage_similaty
         info_mutate_seed[idx-1]["frequency_difference"]=frequency_difference
+        info_mutate_seed[idx-1]["alpha"]=alpha
         generator_input.is_new_idx=False
-        print(info_mutate_seed)
+        
         break
 print(info_mutate_seed)
+
+######### group field
+assertion_field=[]
+raw_data_field=[]
+enumeration_field=[]
+loop_count_field=[]
+offset_field=[]
+size_field=[]
+id_field=[]
+
+
+i=0
+length_seed=input_length-1
+while i<length_seed:
+    done=0
+    for j in range(length_seed-1,i,-1):
+        if check_same_field(i,j,info_mutate_seed):
+            id_field.append([i,j+1])
+            i=j+1
+            done=1
+            break
+    if done==0:
+        id_field.append([i])
+        i+=1
+
+print(id_field)
+########################## Identification field
+
+for offsets in id_field:
+    if len(offsets)!=1:
+        if assertion_field_identification(offsets[0],offsets[1],info_mutate_seed):
+            assertion_field.append(offsets)
+        elif raw_data_field_identification(offsets[0],offsets[1],info_mutate_seed):
+            raw_data_field.append(offsets)
+        elif enumeration_field_indentification(offsets[0],offsets[1],info_mutate_seed):
+            enumeration_field.append(offsets)
+        elif loop_count_field_indentification(offsets[0],offsets[1]+1,info_mutate_seed):
+            loop_count_field.append(offsets)
+        elif offset_field_indentification(offsets[0],offsets[1]+1,info_mutate_seed):
+            offset_field.append(offsets)
+        elif size_field_indentification(offsets[0],offsets[1]+1,info_mutate_seed):
+            size_field.append(offsets)
+
+print(f"assertion: {len(assertion_field)}")
+print(f"raw data: {len(raw_data_field)}")
+print(f"enumeration: {len(enumeration_field)}")
+print(f"loop count: {len(loop_count_field)}")
+print(f"offset: {len(offset_field)}")
+print(f"size: {len(size_field)}")
+
 
 
 
